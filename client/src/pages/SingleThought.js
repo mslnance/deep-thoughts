6ae12a09fb3db378a9e4d583ec40767a48d93c4b
Symphony <55611595+lnance6@users.noreply.github.com>
2021-01-24 20:49:59 -0800

    user based mutations

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -20,6 +22,7 @@ const SingleThought = props => {
     <div>
       <div className="card mb-3">
         <p className="card-header">
+          
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
           </span>{' '}
@@ -31,6 +34,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
